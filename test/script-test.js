ba60e32ba2b77c29d92b0185f18abb0f92ed6568
MASDX <sirawitt42@gmail.com>
2021-02-17 22:10:39 -0500

    Refactor unit test issue with bigNumber

--- test/script-test.js
+++ test/script-test.js
@@ -1,9 +1,10 @@
 const { expect } = require("chai");
 
-describe("SampleToken", function() {
+describe("Initial Coin Offering (ICO) contract", function() {
 
   let token;
   let accounts;
+  const amount = ethers.utils.parseEther("1")
 
   before(async () => {
     const contract = await ethers.getContractFactory("ICO");
@@ -19,37 +20,37 @@ describe("SampleToken", function() {
 
   it("Do not have permission to minting token", async function(){
     const tokenFromOtherWallet = token.connect(accounts[2]);
-    await expect(tokenFromOtherWallet.mint(accounts[2].address,'10')).to.be.reverted
+    await expect(tokenFromOtherWallet.mint(accounts[2].address,amount)).to.be.reverted
   });
 
   it("Do not have permission to burning token", async function(){
     const tokenFromOtherWallet = token.connect(accounts[2]);
-    await expect(tokenFromOtherWallet.burn(accounts[2].address,'10')).to.be.reverted
+    await expect(tokenFromOtherWallet.burn(accounts[2].address,amount)).to.be.reverted
   });
 
-  it("Buy token", async function(){
+  it("Buy token with ether", async function(){
     const tokenFromOtherWallet = token.connect(accounts[2]);
-    const option = {value: ethers.utils.parseEther("1")};
-    const amount = '1000000000000000000000'
+    const option = {value: amount};
+    const wei = ethers.BigNumber.from(option.value)
+    const weiToToken = wei.mul(1000) // 1000 is declare in smart-contract 1 ether * 1000
     await tokenFromOtherWallet.buy(option);
-    expect(await tokenFromOtherWallet.balanceOf(accounts[2].address)).to.equal(amount);
+    expect(await tokenFromOtherWallet.balanceOf(accounts[2].address)).to.equal(weiToToken);
   });
 
   it("Do not have permission to withdraw ether from contract", async function(){
     const tokenFromOtherWallet = token.connect(accounts[2]);
-    const amount = ethers.utils.parseEther("1")
     await expect(tokenFromOtherWallet.withdraw(amount)).to.be.reverted;
   });
 
   it("Transfer adds amount to destination account", async function() {
-    await token.transfer(accounts[1].address,'10');
-    expect(await token.balanceOf(accounts[1].address)).to.equal('10');
+    await token.transfer(accounts[1].address,amount);
+    expect(await token.balanceOf(accounts[1].address)).to.equal(amount);
   });
 
   it("Transfer emits event", async () => {
-    await expect(token.transfer(accounts[1].address, '10'))
+    await expect(token.transfer(accounts[1].address, amount))
       .to.emit(token, "Transfer")
-      .withArgs(accounts[0].address, accounts[1].address, '10');
+      .withArgs(accounts[0].address, accounts[1].address, amount);
   });
 
   it("Can not transfer above the amount", async () => {
@@ -61,35 +62,34 @@ describe("SampleToken", function() {
     const tokenFromOtherWallet = token.connect(accounts[3]);
     await expect(tokenFromOtherWallet.transfer(accounts[0].address, 1)).to.be.reverted;
   });
-
-  //######################## Issue / bug ########################
   
   it("Minting token", async function() {
-    // issue
     const before_mint = await token.balanceOf(accounts[0].address);
-    const amount = '10';
     await token.mint(accounts[0].address,amount);
     const after_burn = await token.balanceOf(accounts[0].address);
-    expect(after_burn).to.equal((before_mint + amount));
+    expect(after_burn).to.equal((before_mint.add(amount)));
   });
 
   it("Burning token", async function() {
-    // issue
-    const before_mint = await token.balanceOf(accounts[0].address);
-    const amount = '10';
-    await token.mint(accounts[0].address,amount);
+    const before_burn = await token.balanceOf(accounts[0].address);
+    await token.burn(accounts[0].address,amount);
     const after_burn = await token.balanceOf(accounts[0].address);
-    expect(after_burn).to.equal((before_mint + amount));
+    expect(after_burn).to.equal((before_burn.sub(amount)));
   });
   
   //######################## TODO ########################
 
   it("Do not have have enough ether to buy token", async function(){
     // TODO
+    const test = ethers.BigNumber.from("999999999999999999999")
+    console.log("🚀 ~ file: script-test.js ~ line 92 ~ it ~ amount", amount.toString())
+    console.log("🚀 ~ file: script-test.js ~ line 91 ~ it ~ test", test.toString())
   });
 
   it("Withdraw ether from contract", async function(){
     // TODO
   });
 
+  1000000000000000000000
+  1000000000000000000
 });
